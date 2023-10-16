c20109cbce4acc0c4c3378e212a8adbae9f5ca67
Daria <nekrasovadasha22@mail.ru>
2023-10-16 22:44:41 +0300

    Add select_genes_from_gbk_to_fasta function

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -2,6 +2,11 @@ import os
 from typing import Tuple
 
 
+def get_last_dict_value(input_dict: dict):
+    keys = list(input_dict.keys())
+    return input_dict[keys[-1]]
+
+
 def convert_multiline_fasta_to_oneline(input_fasta: str,
                                        output_fasta: str = ''):
     if not os.path.exists(input_fasta):
@@ -30,8 +35,9 @@ def convert_multiline_fasta_to_oneline(input_fasta: str,
                 output_fasta_file.write(f'{sequence_lines}\n')
 
 
-def select_genes_from_gbk_to_fasta(input_gbk:str, genes:str, n_before:int = 1,
-                                   n_after:int = 1, output_fasta:str=''):
+def select_genes_from_gbk_to_fasta(input_gbk: str, genes: list[str],
+                                   n_before: int = 1,
+                                   n_after: int = 1, output_fasta: str = ''):
     if not os.path.exists(input_gbk):
         raise ValueError(f'Your {input_gbk} doesn\'t exist')
     if genes == '':
@@ -48,18 +54,66 @@ def select_genes_from_gbk_to_fasta(input_gbk:str, genes:str, n_before:int = 1,
     with open(input_gbk) as input_gbk_file:
         current_line = input_gbk_file.readline()
         while current_line != '':
+
             if current_line.startswith('LOCUS'):
-                parsed_data[current_line.split()[1]] = ('', '', [])
+                parsed_data[current_line.split()[1]] = ['', '', []]
+
             while not current_line.startswith('FEATURES'):
                 current_line = input_gbk_file.readline()
+
             current_line = input_gbk_file.readline()
             separate_line = current_line.split()
-            parsed_data[parsed_data.keys()[-1]][0] = separate_line[1]
-            while True and current_line != '//':
+            get_last_dict_value(parsed_data)[0] = separate_line[1]
+            while not current_line.startswith('ORIGIN'):
                 current_line = input_gbk_file.readline()
                 separate_line = current_line.split()
 
+                if separate_line[0] == 'CDS':
+                    get_last_dict_value(parsed_data)[2] \
+                        .append([separate_line[1], '', ''])
 
+                    current_line = input_gbk_file.readline().strip()
+                    separate_line = current_line.split()
+                    while separate_line[0] != 'CDS':
+                        cds_parameter = separate_line[0].split('=')
+                        if cds_parameter[0] == '/gene':
+                            get_last_dict_value(parsed_data)[2][-1][1] \
+                                = cds_parameter[1].replace('"', '')
+                        if cds_parameter[0] == '/translation':
+                            translation = [cds_parameter[1]]
+                            while not current_line.endswith('"'):
+                                current_line = input_gbk_file.readline().strip()
+                                separate_line = current_line.split()
+                                translation.append(separate_line[0])
+                            translation_line = ''.join(translation)
+                            get_last_dict_value(parsed_data)[2][-1][2] \
+                                = translation_line.replace('"', '')
+                            break
+                        current_line = input_gbk_file.readline()
+                        separate_line = current_line.split()
 
+            current_line = input_gbk_file.readline().strip()
+            while current_line != '//':
+                current_line = input_gbk_file.readline().strip()
+            current_line = input_gbk_file.readline()
 
+            with open(output_fasta, mode="w") as output_fasta_file:
+                for current_key in parsed_data:
+                    for i in range(0, len(parsed_data[current_key][2])):
+                        if any(parsed_data[current_key][2][i][1] == gene for gene in genes):
+                            left_translation = []
+                            right_translation = []
+                            for k in range(i - 1, i - n_before - 1, -1):
+                                if k < 0:
+                                    break
+                                left_translation.append(parsed_data[current_key][2][k][2])
+                            for j in range(i + 1, i + n_after + 1):
+                                if i >= len(parsed_data[current_key][2]):
+                                    break
+                                right_translation.append(parsed_data[current_key][2][j][2])
+                            left_join = ''.join(left_translation[::-1])
+                            right_join = ''.join(right_translation)
+                            merged_translation = left_join + right_join
+                            output_fasta_file.write(current_key + '\n'\
+                                                   + merged_translation + '\n')
 
--- for_test.py
+++ for_test.py
@@ -14,5 +14,18 @@ def open_files(path:str):
 
 
 
-locus = 'LOCUS       NODE_1_length_2558431_cov_75.1851642558431 bp   DNA linear'
-print(locus.split())
+#locus = 'LOCUS       NODE_1_length_2558431_cov_75.1851642558431 bp   DNA linear'
+#print(locus.split())
+
+
+def get_last_dict_value(dict: dict):
+    keys = list(dict.keys())
+    return dict[keys[-1]]
+
+dict = {
+    'aaa': ['1', '2', '1'],
+    'bbb': '2',
+    'ccc': '3',
+}
+
+print(get_last_dict_value(dict))
